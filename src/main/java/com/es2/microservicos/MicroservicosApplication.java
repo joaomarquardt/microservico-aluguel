cde42133505ad91ed7355ad334b873fbf5880c0e
joaomarquardt <marquardt1402@gmail.com>
2025-09-22 22:03:35 -0300

    first commit

---
+++ src/main/java/com/es2/microservicos/MicroservicosApplication.java
@@ -0,0 +1,13 @@
+package com.es2.microservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicosApplication.class, args);
+	}
+
+}
