faf3a2c9839dcb36b15d762d901f2510e8ee36bf
joaomarquardt <marquardt1402@gmail.com>
2025-11-02 20:38:46 -0300

    feat: add method to generate an 'aluguel' without validations

--- src/main/java/com/es2/microservicos/controllers/AluguelController.java
+++ src/main/java/com/es2/microservicos/controllers/AluguelController.java
@@ -1,6 +1,10 @@
 package com.es2.microservicos.controllers;
 
+import com.es2.microservicos.domain.Aluguel;
 import com.es2.microservicos.services.AluguelService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +15,11 @@ public class AluguelController {
 
     public AluguelController(AluguelService aluguelService) {
         this.aluguelService = aluguelService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Aluguel> gerarAluguel() {
+        Aluguel aluguel = aluguelService.gerarAluguel();
+        return new ResponseEntity<>(aluguel, HttpStatus.OK);
     }
 }
--- src/main/java/com/es2/microservicos/services/AluguelService.java
+++ src/main/java/com/es2/microservicos/services/AluguelService.java
@@ -1,5 +1,6 @@
 package com.es2.microservicos.services;
 
+import com.es2.microservicos.domain.Aluguel;
 import com.es2.microservicos.repositories.AluguelRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +10,11 @@ public class AluguelService {
 
     public AluguelService(AluguelRepository aluguelRepository) {
         this.aluguelRepository = aluguelRepository;
+    }
+
+    // TODO: Implementar regras de negócio para geração de aluguel
+    public Aluguel gerarAluguel() {
+        Aluguel aluguel = new Aluguel();
+        return aluguelRepository.save(aluguel);
     }
 }
