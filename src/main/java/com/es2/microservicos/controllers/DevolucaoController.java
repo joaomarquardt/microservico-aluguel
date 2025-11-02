3bc36028c00319c959ff50b539aac59e7d1ad1e8
joaomarquardt <marquardt1402@gmail.com>
2025-11-02 20:51:14 -0300

    refactor: add 'devolucao' controller

---
+++ src/main/java/com/es2/microservicos/controllers/DevolucaoController.java
@@ -0,0 +1,9 @@
+package com.es2.microservicos.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/devolucao")
+public class DevolucaoController {
+}
