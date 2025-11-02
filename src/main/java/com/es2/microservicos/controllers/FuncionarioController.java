74d8483190916fb952057635d58a382a33f8ecde
joaomarquardt <marquardt1402@gmail.com>
2025-11-02 20:16:16 -0300

    feat: add funcionario's CRUD without business rules

--- src/main/java/com/es2/microservicos/controllers/FuncionarioController.java
+++ src/main/java/com/es2/microservicos/controllers/FuncionarioController.java
@@ -1,8 +1,12 @@
 package com.es2.microservicos.controllers;
 
+import com.es2.microservicos.domain.Funcionario;
 import com.es2.microservicos.services.FuncionarioService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/funcionario")
@@ -11,5 +15,35 @@ public class FuncionarioController {
 
     public FuncionarioController(FuncionarioService funcionarioService) {
         this.funcionarioService = funcionarioService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Funcionario>> listarFuncionarios() {
+        List<Funcionario> funcionarios = funcionarioService.listarFuncionarios();
+        return new ResponseEntity<>(funcionarios, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Funcionario> obterFuncionarioPorId(@PathVariable(value = "id") Long id) {
+        Funcionario funcionario = funcionarioService.obterFuncionarioPorId(id);
+        return new ResponseEntity<>(funcionario, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Funcionario> criarFuncionario(@RequestBody Funcionario funcionario) {
+        Funcionario novoFuncionario = funcionarioService.criarFuncionario(funcionario);
+        return new ResponseEntity<>(novoFuncionario, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Funcionario> atualizarFuncionario(@PathVariable(value = "id") Long id, @RequestBody Funcionario funcionarioDetalhes) {
+        Funcionario funcionarioAtualizado = funcionarioService.atualizarFuncionario(id, funcionarioDetalhes);
+        return new ResponseEntity<>(funcionarioAtualizado, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarFuncionario(@PathVariable(value = "id") Long id) {
+        funcionarioService.deletarFuncionario(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/es2/microservicos/services/FuncionarioService.java
+++ src/main/java/com/es2/microservicos/services/FuncionarioService.java
@@ -1,7 +1,11 @@
 package com.es2.microservicos.services;
 
+import com.es2.microservicos.domain.Funcionario;
 import com.es2.microservicos.repositories.FuncionarioRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FuncionarioService {
@@ -9,5 +13,35 @@ public class FuncionarioService {
 
     public FuncionarioService(FuncionarioRepository funcionarioRepository) {
         this.funcionarioRepository = funcionarioRepository;
+    }
+
+    public List<Funcionario> listarFuncionarios() {
+        return funcionarioRepository.findAll();
+    }
+
+    public Funcionario obterFuncionarioPorId(Long id) {
+        return funcionarioRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Funcionário não encontrado!"));
+    }
+
+    public Funcionario criarFuncionario(Funcionario funcionario) {
+        return funcionarioRepository.save(funcionario);
+    }
+
+    public Funcionario atualizarFuncionario(Long id, Funcionario funcionarioDetalhes) {
+        Funcionario funcionarioExistente = funcionarioRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Funcionário não encontrado!"));
+        funcionarioExistente.setMatricula(funcionarioDetalhes.getMatricula());
+        funcionarioExistente.setNome(funcionarioDetalhes.getNome());
+        funcionarioExistente.setEmail(funcionarioDetalhes.getEmail());
+        funcionarioExistente.setSenha(funcionarioDetalhes.getSenha());
+        funcionarioExistente.setConfirmacaoSenha(funcionarioDetalhes.getConfirmacaoSenha());
+        funcionarioExistente.setCpf(funcionarioDetalhes.getCpf());
+        funcionarioExistente.setIdade(funcionarioDetalhes.getIdade());
+        funcionarioExistente.setFuncao(funcionarioDetalhes.getFuncao());
+        return funcionarioRepository.save(funcionarioExistente);
+    }
+
+    public void deletarFuncionario(Long id) {
+        Funcionario funcionarioExistente = funcionarioRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Funcionário não encontrado!"));
+        funcionarioRepository.delete(funcionarioExistente);
     }
 }
