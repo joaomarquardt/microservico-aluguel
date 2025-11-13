d7b472cf864278afc3d7353488483c32c39727b9
joaomarquardt <marquardt1402@gmail.com>
2025-11-12 23:54:08 -0300

    feat: add basic ciclista mapper to convert dtos in entities and vice-versa

---
+++ src/main/java/com/es2/microservicos/mappers/CiclistaMapper.java
@@ -0,0 +1,24 @@
+package com.es2.microservicos.mappers;
+
+import com.es2.microservicos.domain.Ciclista;
+import com.es2.microservicos.dtos.requests.CriarCiclistaRequest;
+import com.es2.microservicos.dtos.responses.CiclistaResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface CiclistaMapper {
+    CiclistaMapper INSTANCE = Mappers.getMapper(CiclistaMapper.class);
+
+    CiclistaResponse toCiclistaResponse(Ciclista ciclista);
+
+    Ciclista toCiclista(CriarCiclistaRequest criarCiclistaRequest);
+
+    List<CiclistaResponse> toCiclistaResponseList(List<Ciclista> ciclistas);
+
+    @Mapping(target = "id", ignore = true)
+    void updateCiclistaFromRequest(CriarCiclistaRequest request, Ciclista ciclista);
+}
