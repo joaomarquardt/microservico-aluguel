6af55bfee31cfd856d90316f4f634b7d4d9ca4d0
joaomarquardt <marquardt1402@gmail.com>
2025-11-12 23:59:20 -0300

    refactor: add 'nascimento' attribute in 'ciclista' creation dto

--- src/main/java/com/es2/microservicos/dtos/requests/CriarCiclistaRequest.java
+++ src/main/java/com/es2/microservicos/dtos/requests/CriarCiclistaRequest.java
@@ -2,9 +2,12 @@ package com.es2.microservicos.dtos.requests;
 
 import com.es2.microservicos.domain.Nacionalidade;
 
+import java.time.LocalDate;
+
 public record CriarCiclistaRequest(
         String nome,
         Nacionalidade nacionalidade,
+        LocalDate nascimento,
         String cpf,
         String email,
         String senha,
