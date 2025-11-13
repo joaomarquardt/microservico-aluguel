dc8b1cb85055e22d549b56befbea0b8a4338a460
joaomarquardt <marquardt1402@gmail.com>
2025-11-13 02:06:17 -0300

    feat: add signature to find 'aluguel' by 'ciclista' id

--- src/main/java/com/es2/microservicos/repositories/AluguelRepository.java
+++ src/main/java/com/es2/microservicos/repositories/AluguelRepository.java
@@ -4,4 +4,5 @@ import com.es2.microservicos.domain.Aluguel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AluguelRepository extends JpaRepository<Aluguel, Long> {
+    Aluguel findByCiclistaId(Long ciclistaId);
 }
