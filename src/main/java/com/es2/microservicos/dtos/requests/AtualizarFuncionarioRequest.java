f47c2d5c37e895fbf4dab31c0e3bafa00958e333
joaomarquardt <marquardt1402@gmail.com>
2025-11-22 11:59:29 -0300

    refactor: remove 'cpf' field from update request dto

--- src/main/java/com/es2/microservicos/dtos/requests/AtualizarFuncionarioRequest.java
+++ src/main/java/com/es2/microservicos/dtos/requests/AtualizarFuncionarioRequest.java
@@ -1,11 +1,12 @@
 package com.es2.microservicos.dtos.requests;
 
+import com.es2.microservicos.domain.Funcao;
+
 public record AtualizarFuncionarioRequest(
         String nome,
         String email,
         String senha,
         String confirmacaoSenha,
-        String cpf,
         Integer idade,
         Funcao funcao
 ) {
