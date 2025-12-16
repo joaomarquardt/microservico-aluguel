475fa82e27dd472812392065265821a71a194e7f
joaomarquardt <marquardt1402@gmail.com>
2025-12-16 15:54:51 -0300

    feat: add exception handler to deal with exception and return better messages

---
+++ src/main/java/com/es2/microservicos/infra/handler/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.es2.microservicos.infra.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, Object>> handleRuntime(RuntimeException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("error", ex.getClass().getSimpleName());
+        body.put("message", ex.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(body);
+    }
+}
